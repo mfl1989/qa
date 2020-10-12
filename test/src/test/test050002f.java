9c98c28b78032dbe9cbc95f4469f0a394f70050e
mfl1989 <784282228qq@gmail.com>
2020-10-12 13:23:46 +0900

    Create test050002f.java

---
+++ test/src/test/test050002f.java
@@ -0,0 +1,21 @@
+package test;
+
+/**
+ * 設問２︓5の階乗を求める(for)。
+ * @author MM
+ *
+ */
+public class test050002f {
+
+	public static void main(String[] args) {
+		int i = 1;//かけざん０がならない
+		//配列の要素を出力する
+		for (int l = 1; l <= 5; l++) {
+			i=i*l;//掛け算求める
+
+		}
+		System.out.println(i);
+
+	}
+
+}
