d07ea9b56619696a8b250a682f56d0e23bc478f5
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:29:57 +0900

    Update test050006.java

--- test/src/test/test050006.java
+++ test/src/test/test050006.java
@@ -3,24 +3,27 @@ package test;
 /**
  *
  * 質問６︓任意⾏列（２次元配列）の積を求める。
- * @author MM
- *
+
+ *@date 作成日:2020/10/12
+ *@date 修正日:2020/10/12
+ *@version 1.1
+ *@author MM
  */
 public class test050006 {
 	public static void main(String[] args) {
-		//二次元配列を設置
-		int[][] arr = new int[][] { { 1, 25, 72, 57, 32, 222 },
-				{ 12234, 43, 3442, 124, 333 }
+		//二次元配列を作成
+		int[][] arr = new int[][] { { 1, 25, 7, 5, 32, 2 },
+				{ 4, 43, 3, 14, 33 }
 		};
-		int avg=1;//初期値０にならない
-		//拡張	for　各要素を出力
-		for(int[]i:arr) {
-			for(int j:i){
+		int mcl = 1;//総的積作成　初期値０にならない
+		//拡張	for　各要素をループ
+		for (int[] i : arr) {
+			for (int j : i) {
 				//積を求める
-				avg=avg*j;
-				System.out.println(avg);
+				mcl = mcl * j;
 			}
 		}
+		System.out.println(mcl);
 	}
 
 }
