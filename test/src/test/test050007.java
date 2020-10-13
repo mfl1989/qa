4eb269fa3ee38426a86ce6a7414ea26efd6ce552
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:30:00 +0900

    Update test050007.java

--- test/src/test/test050007.java
+++ test/src/test/test050007.java
@@ -5,30 +5,33 @@ import java.util.Arrays;
 /**
  * 質問７︓配列を昇順でソートする（java.util等メソッド利⽤禁⽌）。
 	 * int[] arr = new int[]{15, 22, 6, 227, 339, 63, 2, 155};
- * @author MM
- *
+ *@date 作成日:2020/10/12
+ *@date 修正日:2020/10/12
+ *@version 1.1
+ *@author MM
  */
 public class test050007 {
 
 	public static void main(String[] args) {
-		//配列の説明
+		//配列の作成
 		int[] arr = new int[] { 15, 22, 6, 227, 339, 63, 2, 155 };
-		int temp = 0;
-		//二つfor使て各要素を出力
-		for (int i = 0; i < arr.length; i++) {
+
+		//for使てループする
+		for (int i = 0; i < arr.length-1; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
-				//かくようそをたいひする
-				if (arr[i] < arr[j]) {
-					temp=arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
+				//各要素を比較する
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[ j] = temp;
 
 				}
-			}
-			//出力
-			System.out.println(Arrays.toString(arr));
-		}
 
+			}
+
+		}
+		//出力
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
