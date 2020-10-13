580ce5e07b08345e418704cd94af5d8705f06587
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:29:51 +0900

    Update test050004.java

--- test/src/test/test050004.java
+++ test/src/test/test050004.java
@@ -1,26 +1,32 @@
 package test;
+
 /**
  * 質問４︓質問３の配列に重複ある要素を探す（要素の表⽰次数集計必要なし）
  * int[] arr = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+
+ * @date 作成日:2020/10/12
+ * @date 修正日:2020/10/12
+ * @version 1.1
  * @author MM
- *
  */
 public class test050004 {
 
 	public static void main(String[] args) {
-			//配列の声明
-		 int[] arr = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
-		 //配列の出力
-		 for(int i=0;i<arr.length;i++) {
-			 //配列の他要素を出力　対比
-			 for(int j=i+1;j<arr.length;j++) {
-				 //同じ要素を探す
-				 if(arr[i]==arr[j]) {
+		//配列の作成
+		int[] arr = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+		//配列のループ
+		for (int i = 0; i < arr.length; i++) {
+			//配列のループ　比較
+			for (int j = arr.length - 1; j >= 0; j--) {
+				//同じ要素を探す
+				if (arr[i] == arr[j] && i != j) {
 
-					 System.out.println(arr[i]);
-				 }
-			 }
-		 }
+					System.out.println(arr[i]);//出力
+				}
+
+			}
+
+		}
 	}
 
 }
