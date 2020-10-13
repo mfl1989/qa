5fb6979ecc4c42331894518085c8ad2b1030252f
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:30:02 +0900

    Update test050008.java

--- test/src/test/test050008.java
+++ test/src/test/test050008.java
@@ -1,9 +1,12 @@
 package test;
 
 /**
- * LV10のパスカルの三⾓形を出⼒してください
+ * 処理内容：LV10のパスカルの三⾓形を出⼒してください
+
+ * @date 作成日:2020/10/12
+ * @date 修正日:2020/10/12
+ * @version 1.1
  * @author MM
- *
  */
 public class test050008 {
 
@@ -13,24 +16,25 @@ public class test050008 {
 		for (int i = 0; i < arr.length; i++) {
 			//三角形の計算方法
 			arr[i][0] = 1;//両側１なる
-			arr[i][i] = 1;
+			arr[i][i] = 1;//最後の両側も1になる
 		}
-				//第三行列から計算方法
-			for(int i=2;i<arr.length;i++) {
-				for (int j = 1; j < arr[i].length; j++) {
-					arr[i][j] = arr[i - 1][j] + arr[j - 1][i - 1];
-				}
-			}
+		//第三行列ら中間要素の計算
+		for (int i = 2; i < arr.length; i++) {
+			for (int j = 1; j < arr[i].length; j++) {
 
-		//出力の方法
+				arr[i][j] = arr[i - 1][j ] + arr[i - 1][j-1];
+			}
+		}
+
+		//配列　loop
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print(arr[i][j]+"\t");
+			for (int j = 0; j <= i; j++) {
+				//出力要素＋tab
+				System.out.print(arr[i][j] + "\t");
 
-				}
-				System.out.println();
 			}
-
+			System.out.println();//出力改行
 		}
-	}
 
+	}
+}
