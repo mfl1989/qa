711b63dae32d395e14ebb7ac6280bdb256ba8c02
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:29:54 +0900

    Update test050005.java

--- test/src/test/test050005.java
+++ test/src/test/test050005.java
@@ -1,24 +1,28 @@
 package test;
+
 /**
  *  質問５︓質問３の配列に値「７」のインデックスを求む。
  * int[] arr = new int[] { 1, 2, 6, 7, 9, 6, 2, 1116,1122 };
- * @author MM
- *
+
+ *@date 作成日:2020/10/12
+ *@date 修正日:2020/10/12
+ *@version 1.1
+ *@author MM
  */
 public class test050005 {
 
 	public static void main(String[] args) {
-		//声明配列
-		int[] arr = new int[] { 1, 2, 6, 7, 9, 6, 2, 1116 ,1122};
-		//配列出力
-		for(int i=0;i<arr.length;i++) {
+		//配列作成
+		int[] arr = new int[] { 1, 2, 6, 7, 9, 6, 2, 1116, 1122 };
+		//配列loop
+		int index =0;
+		for (int i = 0; i < arr.length; i++) {
 			//配列の要素を指定
-			if(i==7) {
-				System.out.println(arr[i]);
+			if (arr[i] ==7) {
+				index=i;
 			}
-		}
-
-
+			}
+		System.out.println(index);
 
 	}
 
