7c152eb1f82e00b1b1590105457d853beb18fedb
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:29:39 +0900

    Update test050001.java

--- test/src/test/test050001.java
+++ test/src/test/test050001.java
@@ -2,21 +2,24 @@ package test;
 
 /**
  * 設問１︓1から100までの偶数の和を求める(for + if)。
+ *@date 作成日:2020/10/12
+ * @date 修正日:2020/10/12
+ * @version 1.1
  * @author MM
- *
  */
 public class test050001 {
 	public static void main(String[] args) {
-		int l = 0;//設置初期値変数
-		//配列の要素を出力する
+		//偶数初期値設定
+		int sum = 0;
+		//変数　loop
 		for (int i = 0; i <= 100; i++) {
-			//偶数を求める
+			//偶数判断
 			if (i % 2 == 0) {
-				l = l + i;//和を求める
+				sum = sum + i;//和を求める
 
 			}
 		}
 
-		System.out.println(l);
+		System.out.println(sum);
 	}
 }
