4854d20a3c46c55934fb5d2d1ad8c81d94d6c6a3
mfl1989 <784282228qq@gmail.com>
2020-10-13 15:29:48 +0900

    Update test050003.java

--- test/src/test/test050003.java
+++ test/src/test/test050003.java
@@ -2,32 +2,32 @@ package test;
 
 /**
  * 質問３︓配列 {1, 2, 6, 7, 9, 6, 2, 1} の最⼤値、最⼩値、和、平均値を求める。
+
+ *@date 作成日:2020/10/12
+ *@date 修正日:2020/10/12
+ *@version 1.1
  * @author MM
- *
  */
 public class test050003 {
 
 	public static void main(String[] args) {
-		int arr[] = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+		int []array = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
 		//初期値設定
-		int max = arr[0];
-		int min = arr[0];
+		int max = array[0];
+		int min = array[0];
 		int sum = 0;
-		int avg = 1;
-		//配列の出力
-		for (int i = 0; i < arr.length; i++) {
-			/*for(int j=0;j<arr.length;j++) {
-			 if(arr[i]<arr[j]) {
-				 arr[j]=arr[i];
-				 System.out.println(arr[i]);
-			 }*/
+		float avg = 1;
+		//配列のloop
+
+		for (int i = 0; i < array.length; i++) {
+
 			//各値を求める
-			sum=sum+arr[i];
-			avg=avg*arr[i];
-			if (max< arr[i]) {
-				max = arr[i];
-				if(min>arr[i]) {
-					min=arr[i];
+			sum=sum+array[i];
+			avg=sum/array.length;
+			if (max< array[i]) {
+				max = array[i];
+				if(min>array[i]) {
+					min=array[i];
 				}
 
 			}
